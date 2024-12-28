f632615fbc9f117165e10e895a57d63c8797ec2b
Noah Giboney <noahgiboney@gmail.com>
2024-12-28 13:54:52 -0800

    setup charkara ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import Navbar from "@/components/Navbar";
+import { Button } from "@/components/ui/button";
+import { HStack } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <main>
-      <Navbar />
-      <h1 className="text-6xl">Home</h1>
-    </main>
+    <HStack wrap="wrap" gap="10">
+      <Button size="xs">Button (xs)</Button>
+      <Button size="sm">Button (sm)</Button>
+      <Button size="md">Button (md)</Button>
+      <Button size="lg">Button (lg)</Button>
+      <Button size="xl">Button (xl)</Button>
+    </HStack>
   );
 }
