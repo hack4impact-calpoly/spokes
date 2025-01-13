0cb935ed93967e4bcaa466b6e752869f98dc7ad3
Mark McGuire <mcguirenumber1@gmail.com>
2025-01-13 09:53:32 -0800

    fix(job schema): converted var names to camelCase

--- src/database/jobSchema.ts
+++ src/database/jobSchema.ts
@@ -2,39 +2,39 @@ import mongoose, { Schema } from "mongoose";
 
 // Enum for the employment type and compensation type
 export enum EmploymentType {
-  PART_TIME = "part-time",
-  FULL_TIME = "full time",
-  VOLUNTEER = "volunteer",
+  partTime = "part-time",
+  fullTime = "full time",
+  volunteer = "volunteer",
 }
 
 export enum CompensationType {
-  PAID = "paid",
-  NON_PAID = "non-paid",
+  paid = "paid",
+  nonPaid = "non-paid",
 }
 
 // Interface for the job object
 export interface IJob {
-  organization_name: string;
-  organization_industry: string;
+  organizationName: string;
+  organizationIndustry: string;
   title: string;
-  post_date: Date;
-  expire_date: Date;
-  job_description: string;
-  employment_type: EmploymentType;
-  compensation_type: CompensationType;
+  postDate: Date;
+  expireDate: Date;
+  jobDescription: string;
+  employmentType: EmploymentType;
+  compensationType: CompensationType;
   url: string;
 }
 
 // Schema for the job object
 const JobSchema = new Schema({
-  organization_name: { type: String, required: true },
-  organization_industry: { type: String, required: true },
+  organizationName: { type: String, required: true },
+  organizationIndustry: { type: String, required: true },
   title: { type: String, required: true },
-  post_date: { type: Date, required: true },
-  expire_date: { type: Date, required: true },
-  job_description: { type: String, required: true },
-  employment_type: { type: EmploymentType, required: true },
-  compensation_type: { type: CompensationType, required: true },
+  postDate: { type: Date, required: true },
+  expireDate: { type: Date, required: true },
+  jobDescription: { type: String, required: true },
+  employmentType: { type: EmploymentType, required: true },
+  compensationType: { type: CompensationType, required: true },
   url: { type: String, required: true },
 });
 
