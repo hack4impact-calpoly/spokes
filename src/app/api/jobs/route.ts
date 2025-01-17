4180bdd5c7f08591eaf7f82ba67f9915bbc0318f
Deep-Singh14 <khushdip14@gmail.com>
2025-01-16 17:51:15 -0800

    file: api/jobs/route.ts  feat: DELETE method

---
+++ src/app/api/jobs/route.ts
@@ -0,0 +1,28 @@
+import connectDB from "@/database/db";
+import { NextRequest, NextResponse } from "next/server";
+import jobSchema from "@/database/jobSchema";
+
+export async function DELETE(_request: NextRequest, { params }: { params: { _id: string } }) {
+  try {
+    await connectDB();
+    const id = params._id;
+
+    //Requires id
+    if (!id) {
+      return NextResponse.json({ error: "ID is required" }, { status: 400 });
+    }
+
+    //Deleting based on _id
+    const result = await jobSchema.findByIdAndDelete(id);
+
+    //if id not found
+    if (!result) {
+      return NextResponse.json({ error: "Job not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ message: "Job deleted successfully" }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "An error occurred while deleting the job" }, { status: 500 });
+  }
+}
