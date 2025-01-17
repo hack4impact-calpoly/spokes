b657c27ce21a4bed23c9af61073f575a5754c1ff
Mark McGuire <mcguirenumber1@gmail.com>
2025-01-16 23:54:36 -0800

    feat(job card): 2nd attempt to remove chakra ui components from file structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import { HStack, Button } from "@chakra-ui/react";
+import JobCard from "@/components/JobCard";
 
 export default function Home() {
   return (
-    <HStack wrap="wrap" gap="10">
-      <Button size="xs">Button (xs)</Button>
-      <Button size="sm">Button (sm)</Button>
-      <Button size="md">Button (md)</Button>
-      <Button size="lg">Button (lg)</Button>
-      <p className="text-3xl text-blue-500">Tailwind styled</p>
-    </HStack>
+    <div>
+      <JobCard></JobCard>
+    </div>
   );
 }
