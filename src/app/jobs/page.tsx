fc7c99be36ea1fd60460cb5cfc931668aa4098b5
Matthew Blam <blammatthew@gmail.com>
2025-01-17 16:06:51 -0800

    feat: add job cards and responsive layout

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,20 +1,60 @@
 "use client";
 import Navbar from "@/components/Navbar";
-import { Checkbox } from "@/components/Checkbox";
 import { FilterCard } from "@/components/FilterCard";
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+import JobCard from "@/components/JobCard";
 
 export default function Jobs() {
+  const [tab, setTab] = useState(1);
   return (
     <div className="w-full h-full">
       <Navbar></Navbar>
-      <div className="mt-20 px-20">
-        <div className="inline-block">
-          <div className="text-black font-semibold text-3xl mb-4">Filters</div>
+      <div className="mt-20 px-8 md:px-16 lg:px-20 flex flex-col lg:flex-row gap-16 lg:gap-8">
+        <div>
+          <div className="text-black font-semibold text-3xl mb-2 lg:mb-6">Filters</div>
           <div>
             <FilterCard></FilterCard>
           </div>
         </div>
-        <div className="inline-block"></div>
+        <div className="w-full">
+          <div className="flex gap-8 w-full">
+            <div
+              className={twMerge(
+                "text-black text-3xl mb-2 lg:mb-6 cursor-pointer",
+                tab == 1 ? "font-semibold" : "font-normal text-[#C3C3C3]",
+              )}
+              onClick={() => {
+                // Later add functionally to display listings
+                setTab(1);
+              }}
+            >
+              All Jobs
+            </div>
+            <div
+              className={twMerge(
+                "text-black text-3xl mb-6 cursor-pointer",
+                tab == 2 ? "font-semibold" : "font-normal text-[#C3C3C3]",
+              )}
+              onClick={() => {
+                // Later add functionally to display listings
+                setTab(2);
+              }}
+            >
+              Recently Viewed
+            </div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <JobCard></JobCard>
+            <JobCard></JobCard>
+            <JobCard></JobCard>
+            <JobCard></JobCard>
+            <JobCard></JobCard>
+            <JobCard></JobCard>
+            <JobCard></JobCard>
+            <JobCard></JobCard>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/FilterCard.tsx
+++ src/components/FilterCard.tsx
@@ -12,7 +12,11 @@ export const FilterCard = forwardRef<HTMLDivElement, FilterCardProps>(({ childre
   const [paidChecked, setPaidChecked] = useState(false);
   const [volunteerChecked, setVolunteerChecked] = useState(false);
   return (
-    <div ref={ref} className={twMerge("bg-[#F7F7F7] px-8 py-6 rounded flex flex-col gap-4", className)} {...props}>
+    <div
+      ref={ref}
+      className={twMerge("bg-[#F7F7F7] px-8 py-6 rounded flex lg:flex-col gap-16 lg:gap-4", className)}
+      {...props}
+    >
       <div>
         <div className="text-black font-semibold text-lg mb-1">Employment</div>
         <div className="flex flex-col gap-[2px]">
@@ -34,7 +38,7 @@ export const FilterCard = forwardRef<HTMLDivElement, FilterCardProps>(({ childre
           ></Checkbox>
         </div>
       </div>
-      <div className="mb-8">
+      <div className="lg:mb-8">
         <div className="text-black font-semibold text-lg mb-1">Compensation</div>
         <div className="flex flex-col gap-[2px]">
           <Checkbox
