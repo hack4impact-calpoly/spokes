f75c39794093a2facc93fe884d533875f33e8909
Nolan Knievel <nolan.knievel@icloud.com>
2025-01-20 14:40:16 -0800

    feat: validate job object, add job to db

--- src/app/api/jobs/route.ts
+++ src/app/api/jobs/route.ts
@@ -10,4 +10,32 @@ import Job from "@/database/jobSchema";
  */
 export async function PUT(request: Request) {
   await connectDB();
+
+  // Get the job object from the request body
+  const { job }: { job: IJob } = await request.json();
+
+  // Validate the job object
+  if (
+    !job ||
+    !job.organizationName ||
+    !job.organizationIndustry ||
+    !job.title ||
+    !job.postDate ||
+    !job.expireDate ||
+    !job.jobDescription ||
+    !job.employmentType ||
+    !job.compensationType ||
+    !job.url ||
+    !job._id
+  ) {
+    return NextResponse.json({ message: "Invalid job provided" }, { status: 400 });
+  }
+
+  // Add job to database
+  const res = await Job.updateOne({ _id: job._id }, job);
+  if (!res) {
+    return NextResponse.json({ message: "Error updating job" }, { status: 500 });
+  } else {
+    return NextResponse.json({ message: "Hello from the API!" });
+  }
 }
