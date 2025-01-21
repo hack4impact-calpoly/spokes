37a5f75913f70f97c0515bf655de1edb924cb7fa
Noah Giboney <noahgiboney@gmail.com>
2025-01-21 13:43:30 -0800

    feat: setup page redirect to /jobs

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
-const { chakra } = require("@chakra-ui/react");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/jobs",
+        permanent: true,
+      },
+    ];
   },
 };
 
