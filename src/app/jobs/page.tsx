a242f61ae5af5255440d7c837ac9f0a4bad0faeb
Matthew Blam <blammatthew@gmail.com>
2025-01-21 10:42:00 -0800

    feat: fetch job page data and display with loader

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,58 +1,43 @@
 "use client";
 import Navbar from "@/components/Navbar";
 import { FilterCard } from "@/components/FilterCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { twMerge } from "tailwind-merge";
 import JobGrid from "@/components/JobGrid";
 import { IJob } from "@/database/jobSchema";
+import { Spinner } from "@chakra-ui/react";
 
 export default function Jobs() {
   const [tab, setTab] = useState(1);
 
-  const testJobs: IJob[] = [
-    {
-      _id: "1",
-      organizationName: "TechSolutions",
-      organizationIndustry: "Technology",
-      title: "Frontend Developer",
-      postDate: new Date("2023-11-01"),
-      expireDate: new Date("2023-12-01"),
-      jobDescription: "Develop and maintain user interfaces using React.",
-      employmentType: "Full-Time",
-      compensationType: "Paid",
-      jobStatus: "Approved",
-      url: "https://techsolutions.com/jobs/1",
-    },
-    {
-      _id: "2",
-      organizationName: "GreenFuture",
-      organizationIndustry: "Environmental Services",
-      title: "Environmental Scientist",
-      postDate: new Date("2023-10-15"),
-      expireDate: new Date("2023-11-15"),
-      jobDescription: "Research and develop solutions for environmental challenges.",
-      employmentType: "Part Time",
-      compensationType: "Non-Paid",
-      jobStatus: "Pending",
-      url: "https://greenfuture.com/jobs/2",
-    },
-  ];
+  const [jobData, setJobData] = useState<null | IJob[]>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("/api/jobs");
+      const result = await response.json();
+      console.log(result);
+      setJobData(result);
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-screen">
       <Navbar></Navbar>
       <div className="mt-20 px-8 md:px-16 lg:px-20 flex flex-col lg:flex-row gap-16 lg:gap-8">
-        <div>
-          <div className="text-black font-semibold text-3xl mb-4 lg:mb-6 select-none">Filters</div>
+        <div className="flex flex-col gap-4 lg:gap-6">
+          <div className="text-black font-semibold text-3xl select-none">Filters</div>
           <div>
             <FilterCard></FilterCard>
           </div>
         </div>
-        <div className="w-full">
+        <div className="w-full flex flex-col gap-4 lg:gap-6">
           <div className="flex gap-8 w-full">
             <div
               className={twMerge(
-                "text-black text-3xl mb-4 lg:mb-6 cursor-pointer select-none",
+                "text-black text-3xl cursor-pointer select-none",
                 tab == 1 ? "font-semibold" : "font-normal text-[#C3C3C3]",
               )}
               onClick={() => {
@@ -64,7 +49,7 @@ export default function Jobs() {
             </div>
             <div
               className={twMerge(
-                "text-black text-3xl mb-4 lg:mb-6 cursor-pointer select-none",
+                "text-black text-3xl cursor-pointer select-none",
                 tab == 2 ? "font-semibold" : "font-normal text-[#C3C3C3]",
               )}
               onClick={() => {
@@ -75,7 +60,14 @@ export default function Jobs() {
               Recently Viewed
             </div>
           </div>
-          <JobGrid jobs={testJobs} />
+          {/* Conditional rendering for jobData with loader as fallback */}
+          {jobData ? (
+            <JobGrid jobs={jobData} />
+          ) : (
+            <div className="grow flex justify-center items-center lg:mt-0 mt-24">
+              <Spinner className="" size="xl"></Spinner>
+            </div>
+          )}
         </div>
       </div>
     </div>
