7c4fd5c96e9ce5e33e28e22b4e7f7c49b9f34a17
sgill33 <sgill33@calpoly.edu>
2025-01-21 20:16:26 -0800

    fix: update return type & remove unused import

--- src/app/api/jobs/route.ts
+++ src/app/api/jobs/route.ts
@@ -2,12 +2,11 @@ import connectDB from "@/database/db";
 import { NextResponse } from "next/server";
 import { IJob } from "@/database/jobSchema";
 import Job from "@/database/jobSchema";
-import mongoose from "mongoose";
 
 /**
  * PUT API route
  * accepts a job object and updates the job in the database
- * @returns {message: string}
+ * @returns {Promise<NextResponse>}
  */
 export async function PUT(request: Request) {
   try {
