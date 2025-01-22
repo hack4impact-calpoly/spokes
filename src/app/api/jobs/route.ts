7b14ab89d70a88c87012e1cbc2a6e3a675c4a43f
Nolan Knievel <nolan.knievel@icloud.com>
2025-01-21 18:10:04 -0800

    feat: add try catch block around code, now correctly fails when id DNE in db

--- src/app/api/jobs/route.ts
+++ src/app/api/jobs/route.ts
@@ -2,6 +2,7 @@ import connectDB from "@/database/db";
 import { NextResponse } from "next/server";
 import { IJob } from "@/database/jobSchema";
 import Job from "@/database/jobSchema";
+import mongoose from "mongoose";
 
 /**
  * PUT API route
@@ -9,16 +10,14 @@ import Job from "@/database/jobSchema";
  * @returns {message: string}
  */
 export async function PUT(request: Request) {
-  await connectDB();
-  console.log("connected to database");
-  // Get the job object from the request body
-  const job: IJob = await request.json();
+  try {
+    await connectDB();
 
-  // Add job to database
-  const res = await Job.updateOne({ _id: job._id }, job);
-  if (!res) {
-    return NextResponse.json({ message: "Error updating job" }, { status: 500 });
-  } else {
-    return NextResponse.json({ message: "Job added successfully" });
+    const job: IJob = await request.json();
+
+    await Job.findByIdAndUpdate(job._id, job, { new: true }).orFail(new Error("Job not found"));
+    return NextResponse.json({ message: "Job updated successfully" });
+  } catch (error: any) {
+    return NextResponse.json({ message: "Error updating job: ", error }, { status: 500 });
   }
 }
