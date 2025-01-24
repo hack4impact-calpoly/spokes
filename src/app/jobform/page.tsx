535ff4efc83339f76749173098340046b7086852
Kyler <kylernord@Kylers-MacBook-Pro.local>
2025-01-23 22:37:59 -0800

    feat: finished the job form

--- src/app/jobform/page.tsx
+++ src/app/jobform/page.tsx
@@ -7,14 +7,21 @@ const JobFormPage: React.FC = () => {
       <Heading as="h1" size="xl" mb={4}>
         <strong>Create new listing</strong>
       </Heading>
+      <Heading as="h2" size="lg" mb={6}>
+        Job Information
+      </Heading>
       <Heading as="h2" size="md" mb={2}>
         Organization name
       </Heading>
-      <Input placeholder="Enter your response" mb={4} />
+      <Input placeholder="Enter your response" mb={6} />
+      <Heading as="h2" size="md" mb={2}>
+        Organization Industry
+      </Heading>
+      <Input placeholder="Enter your response" mb={6} />
       <Heading as="h2" size="md" mb={2}>
         Job title
       </Heading>
-      <Input placeholder="Enter your response" mb={4} />
+      <Input placeholder="Enter your response" mb={6} />
       <Heading as="h2" size="md" mb={2}>
         Select all that apply
       </Heading>
@@ -35,21 +42,29 @@ const JobFormPage: React.FC = () => {
           Full-time
         </Button>
       </Stack>
-      <Flex mb={4}>
+      <Flex mb={6}>
         <Box flex="1" mr={2}>
-          <Heading as="h2" size="md" mb={2}>
+          <Heading as="h2" size="md" mb={6}>
             Posting Date
           </Heading>
           <Input placeholder="xx/xx/xxxx" />
         </Box>
         <Box flex="1" ml={2}>
-          <Heading as="h2" size="md" mb={2}>
+          <Heading as="h2" size="md" mb={6}>
             Expiration Date
           </Heading>
           <Input placeholder="xx/xx/xxxx" />
         </Box>
       </Flex>
       <Heading as="h2" size="md" mb={2}>
+        Job Description
+      </Heading>
+      <Input placeholder="Enter your response" mb={6} />
+      <Heading as="h2" size="md" mb={2}>
+        Link to job listing
+      </Heading>
+      <Input placeholder="xxxx.com" mb={6} />
+      <Heading as="h2" size="md" mb={6}>
         Personal Information
       </Heading>
       <Flex mb={4}>
@@ -57,7 +72,7 @@ const JobFormPage: React.FC = () => {
           <Heading as="h2" size="md" mb={2}>
             Name
           </Heading>
-          <Input placeholder="First and Last Name" />
+          <Input placeholder="First and Last Name" mb={6} />
         </Box>
         <Box flex="1" ml={2}>
           <Heading as="h2" size="md" mb={2}>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,110 +1,13 @@
-// import { HStack, Button } from "@chakra-ui/react";
+import { HStack, Button } from "@chakra-ui/react";
 
-// export default function Home() {
-//   return (
-//     <HStack wrap="wrap" gap="10">
-//       <Button size="xs">Button (xs)</Button>
-//       <Button size="sm">Button (sm)</Button>
-//       <Button size="md">Button (md)</Button>
-//       <Button size="lg">Button (lg)</Button>
-//       <p className="text-3xl text-blue-500">Tailwind styled</p>
-//     </HStack>
-//   );
-// }
-import React from "react";
-import { Box, Button, Flex, Heading, Input, Stack } from "@chakra-ui/react";
-
-const JobFormPage: React.FC = () => {
+export default function Home() {
   return (
-    <Box p={5}>
-      <Heading as="h1" size="xl" mb={4}>
-        <strong>Create new listing</strong>
-      </Heading>
-      <Heading as="h2" size="lg" mb={6}>
-        Job Information
-      </Heading>
-      <Heading as="h2" size="md" mb={2}>
-        Organization name
-      </Heading>
-      <Input placeholder="Enter your response" mb={6} />
-      <Heading as="h2" size="md" mb={2}>
-        Organization Industry
-      </Heading>
-      <Input placeholder="Enter your response" mb={6} />
-      <Heading as="h2" size="md" mb={2}>
-        Job title
-      </Heading>
-      <Input placeholder="Enter your response" mb={6} />
-      <Heading as="h2" size="md" mb={2}>
-        Select all that apply
-      </Heading>
-      <Stack spacing={2} direction="row" mb={4}>
-        <Button bg="green.200" color="black" p={2} borderWidth="1px" borderRadius="md">
-          Paid
-        </Button>
-        <Button bg="red.200" color="black" p={2} borderWidth="1px" borderRadius="md">
-          Not Paid
-        </Button>
-        <Button bg="blue.200" color="black" p={2} borderWidth="1px" borderRadius="md">
-          Volunteer
-        </Button>
-        <Button bg="red.200" color="black" p={2} borderWidth="1px" borderRadius="md">
-          Part-time
-        </Button>
-        <Button bg="yellow.200" color="black" p={2} borderWidth="1px" borderRadius="md">
-          Full-time
-        </Button>
-      </Stack>
-      <Flex mb={6}>
-        <Box flex="1" mr={2}>
-          <Heading as="h2" size="md" mb={6}>
-            Posting Date
-          </Heading>
-          <Input placeholder="xx/xx/xxxx" />
-        </Box>
-        <Box flex="1" ml={2}>
-          <Heading as="h2" size="md" mb={6}>
-            Expiration Date
-          </Heading>
-          <Input placeholder="xx/xx/xxxx" />
-        </Box>
-      </Flex>
-      <Heading as="h2" size="md" mb={2}>
-        Job Description
-      </Heading>
-      <Input placeholder="Enter your response" mb={6} />
-      <Heading as="h2" size="md" mb={2}>
-        Link to job listing
-      </Heading>
-      <Input placeholder="xxxx.com" mb={6} />
-      <Heading as="h2" size="md" mb={6}>
-        Personal Information
-      </Heading>
-      <Flex mb={4}>
-        <Box flex="1" mr={2}>
-          <Heading as="h2" size="md" mb={2}>
-            Name
-          </Heading>
-          <Input placeholder="First and Last Name" mb={6} />
-        </Box>
-        <Box flex="1" ml={2}>
-          <Heading as="h2" size="md" mb={2}>
-            Phone Number
-          </Heading>
-          <Input placeholder="Phone Number" />
-        </Box>
-      </Flex>
-      <Heading as="h2" size="md" mb={2}>
-        Email
-      </Heading>
-      <Input placeholder="xxxxx@example.com" mb={4} />
-      <Flex justify="center" mt={4}>
-        <Button colorScheme="blackAlpha" size="lg">
-          Submit
-        </Button>
-      </Flex>
-    </Box>
+    <HStack wrap="wrap" gap="10">
+      <Button size="xs">Button (xs)</Button>
+      <Button size="sm">Button (sm)</Button>
+      <Button size="md">Button (md)</Button>
+      <Button size="lg">Button (lg)</Button>
+      <p className="text-3xl text-blue-500">Tailwind styled</p>
+    </HStack>
   );
-};
-
-export default JobFormPage;
+}
