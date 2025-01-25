6a6e7c8cdc989046277a721236ddf90bc99906fb
Noah Giboney <noahgiboney@gmail.com>
2025-01-25 14:45:43 -0800

    feat: job posted date

--- src/components/JobCard/AdminCard.tsx
+++ src/components/JobCard/AdminCard.tsx
@@ -3,6 +3,7 @@ import { IJob } from "@/database/jobSchema";
 import AdminJobBadge from "@/components/JobCard/AdminJobBadge";
 import JobBadge from "@/components/JobCard/JobBadge";
 import JobCardInformation from "@/components/JobCard/JobCardInformation";
+import JobPostedDate from "@/components/JobCard/JobPostedDate";
 
 interface JobCardProps {
   job: IJob;
@@ -11,23 +12,13 @@ interface JobCardProps {
 export default function AdminCard({ job }: JobCardProps) {
   return (
     <div className="max-w-[100%]">
-      <div className="bg-[#f7f7f7] rounded-lg px-8 pt-2 pb-8 mb-4">
-        <div className="my-5">
-          <div className="flex flex-row-reverse mb-5">
-            <Icon viewBox="0 0 200 200" color="#A1E3CB">
-              {/* This little dot will also need to be made dynamic with member/non-badge member when that is made */}
-              <path fill="currentColor" d="M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0" />
-            </Icon>
-          </div>
-          <div className="flex justify-between">
-            <AdminJobBadge jobStatus={job.jobStatus} />
-            {/* Currently dont have member/non-member data. Can add it here once done. Leaving it as jobStatus for now */}
-            <AdminJobBadge jobStatus={job.jobStatus} />
-          </div>
+      <div className="bg-[#f7f7f7] rounded-3xl px-8 py-5 shadow-sm">
+        <div className="flex justify-between mb-5">
+          <AdminJobBadge jobStatus={job.jobStatus} />
         </div>
         <JobCardInformation job={job} />
-        <div className="flex flex-wrap justify-between min-[1000px]:flex-row flex-col min-[1000px]:gap-4 gap-2">
-          <div className="flex flex-wrap gap-3">
+        <div className="flex flex-wrap justify-between min-[1000px]:flex-row flex-col min-[1000px]:gap-4 gap-2 mt-5">
+          <div className="flex flex-wrap gap-2">
             <JobBadge badgeType={job.employmentType} />
             <JobBadge badgeType={job.compensationType} />
           </div>
@@ -44,6 +35,9 @@ export default function AdminCard({ job }: JobCardProps) {
               Edit Application
             </Button>
           </div>
+        </div>
+        <div className="mt-5">
+          <JobPostedDate date={job.postDate} />
         </div>
       </div>
     </div>
--- src/components/JobCard/JobCard.tsx
+++ src/components/JobCard/JobCard.tsx
@@ -2,6 +2,7 @@ import { Button } from "@chakra-ui/react";
 import { IJob } from "@/database/jobSchema";
 import JobBadge from "@/components/JobCard/JobBadge";
 import JobCardInformation from "@/components/JobCard/JobCardInformation";
+import JobPostedDate from "@/components/JobCard/JobPostedDate";
 
 interface JobCardProps {
   job: IJob;
@@ -10,11 +11,12 @@ interface JobCardProps {
 export default function JobCard({ job }: JobCardProps) {
   return (
     <div className="max-w-[100%]">
-      <div className="bg-[#f7f7f7] rounded px-8 pt-6 pb-8 mb-4">
+      <div className="bg-[#f7f7f7] rounded-md px-8 pt-5 pb-2 shadow-sm">
         <JobCardInformation job={job} />
-        <div className="flex lg:flex-row flex-col gap-3">
+        <div className="flex flex-row md:flex-col lg:flex-row gap-2 items-end lg:items-end md:items-start mt-5">
           <JobBadge badgeType={job.employmentType} />
           <JobBadge badgeType={job.compensationType} />
+          <JobPostedDate date={job.postDate} />
         </div>
         <div className="flex lg:flex-row flex-col gap-4 my-5">
           <Button className="lg:w-[50%] w-full" fontWeight="normal" variant="outline" borderColor="black">
