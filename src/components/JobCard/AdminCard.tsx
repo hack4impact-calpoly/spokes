42d3ecdbe43633c3da6b9ffc2e440f4577b81423
Noah Giboney <noahgiboney@gmail.com>
2025-01-25 16:17:33 -0800

    chore: rename

--- src/components/JobCard/AdminCard.tsx
+++ src/components/JobCard/AdminCard.tsx
@@ -1,6 +1,6 @@
 import { Button, Icon } from "@chakra-ui/react";
 import { IJob } from "@/database/jobSchema";
-import AdminJobBadge from "@/components/JobCard/AdminJobBadge";
+import JobStatusBadge from "@/components/JobCard/JobStatusBadge";
 import JobBadge from "@/components/JobCard/JobBadge";
 import JobCardInformation from "@/components/JobCard/JobCardInformation";
 import JobPostedDate from "@/components/JobCard/JobPostedDate";
@@ -14,7 +14,7 @@ export default function AdminCard({ job }: JobCardProps) {
     <div className="max-w-[100%]">
       <div className="bg-[#f7f7f7] rounded-3xl px-8 py-5 shadow-sm">
         <div className="flex justify-between mb-5">
-          <AdminJobBadge jobStatus={job.jobStatus} />
+          <JobStatusBadge jobStatus={job.jobStatus} />
         </div>
         <JobCardInformation job={job} />
         <div className="flex flex-wrap justify-between min-[1000px]:flex-row flex-col min-[1000px]:gap-4 gap-2 mt-5">
