1113d1a7c6a7e5087edfd237e40191534bedfc16
Nolan Knievel <nolan.knievel@icloud.com>
2025-02-02 16:28:55 -0800

    feat: wrap app body interior with ReactQueryProvier for global access

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Providers from "./ui/providers";
 import { Inter } from "next/font/google";
 import NavBar from "@/components/NavBar/NavBar";
+import ReactQueryProvider from "@/components/ReactQueryProvider";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,10 +28,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     >
       <html lang="en">
         <body className={inter.className}>
-          <Providers>
-            <NavBar />
-            {children}
-          </Providers>
+          <ReactQueryProvider>
+            <Providers>
+              <NavBar />
+              {children}
+            </Providers>
+            <ReactQueryDevtools />
+          </ReactQueryProvider>
         </body>
       </html>
     </ClerkProvider>
