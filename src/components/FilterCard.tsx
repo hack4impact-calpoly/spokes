3b60935a15ceaaae082bdb73b82612a5c4bab040
Ellie Pearson <ellie.jm.pearson@gmail.com>
2025-02-14 22:11:17 -0800

    refactor: update filterCard

--- src/components/FilterCard.tsx
+++ src/components/FilterCard.tsx
@@ -16,9 +16,9 @@ export const FilterCard = forwardRef<HTMLDivElement, FilterCardProps>(
   ({ onFilterChange, children, className, ...props }, ref) => {
     const [fullTimeChecked, setFullTimeChecked] = useState(false);
     const [partTimeChecked, setPartTimeChecked] = useState(false);
+    const [salaryChecked, setSalaryChecked] = useState(false);
+    const [hourlyChecked, setHourlyChecked] = useState(false);
     const [volunteerChecked, setVolunteerChecked] = useState(false);
-    const [paidChecked, setPaidChecked] = useState(false);
-    const [unpaidChecked, setUnpaidChecked] = useState(false);
 
     return (
       <div className="sticky top-[155px]">
@@ -31,7 +31,7 @@ export const FilterCard = forwardRef<HTMLDivElement, FilterCardProps>(
           {...props}
         >
           <div>
-            <div className="text-black font-semibold text-lg mb-1 select-none">Employment</div>
+            <div className="mb-1 text-lg font-semibold text-black select-none">Employment</div>
             <div className="flex flex-col gap-[2px]">
               <Checkbox
                 label="Full-Time"
@@ -49,33 +49,33 @@ export const FilterCard = forwardRef<HTMLDivElement, FilterCardProps>(
                   onFilterChange("employment", "part-time");
                 }}
               ></Checkbox>
+            </div>
+          </div>
+          <div className="lg:mb-8">
+            <div className="mb-1 text-lg font-semibold text-black select-none">Compensation</div>
+            <div className="flex flex-col gap-[2px]">
+              <Checkbox
+                label="Salary"
+                checked={salaryChecked}
+                changeHandler={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  setSalaryChecked(event.target.checked);
+                  onFilterChange("compensation", "salary");
+                }}
+              ></Checkbox>
+              <Checkbox
+                label="Hourly"
+                checked={hourlyChecked}
+                changeHandler={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  setHourlyChecked(event.target.checked);
+                  onFilterChange("compensation", "hourly");
+                }}
+              ></Checkbox>
               <Checkbox
                 label="Volunteer"
                 checked={volunteerChecked}
                 changeHandler={(event: React.ChangeEvent<HTMLInputElement>) => {
                   setVolunteerChecked(event.target.checked);
-                  onFilterChange("employment", "volunteer");
-                }}
-              ></Checkbox>
-            </div>
-          </div>
-          <div className="lg:mb-8">
-            <div className="text-black font-semibold text-lg mb-1 select-none">Compensation</div>
-            <div className="flex flex-col gap-[2px]">
-              <Checkbox
-                label="Paid"
-                checked={paidChecked}
-                changeHandler={(event: React.ChangeEvent<HTMLInputElement>) => {
-                  setPaidChecked(event.target.checked);
-                  onFilterChange("compensation", "paid");
-                }}
-              ></Checkbox>
-              <Checkbox
-                label="Unpaid"
-                checked={unpaidChecked}
-                changeHandler={(event: React.ChangeEvent<HTMLInputElement>) => {
-                  setUnpaidChecked(event.target.checked);
-                  onFilterChange("compensation", "unpaid");
+                  onFilterChange("compensation", "volunteer");
                 }}
               ></Checkbox>
             </div>
