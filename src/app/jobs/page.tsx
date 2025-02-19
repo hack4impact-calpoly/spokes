a0bdbbb3c227fa35c0b07b3b64426c2334cd55e0
sgill33 <sgill33@calpoly.edu>
2025-02-19 14:24:16 -0800

    fix: added bottom padding to jobs page

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -172,7 +172,7 @@ export default function Jobs() {
   }, [inView, hasNextPage, fetchNextPage]);
 
   return (
-    <div className="w-full flex flex-col">
+    <div className="w-full flex flex-col pb-10">
       <div className="mt-[50px] px-8 md:px-16 lg:px-20 flex flex-col lg:flex-row gap-16 lg:gap-8 grow">
         <div className="flex flex-col gap-4 lg:gap-6">
           <div className="text-black font-semibold text-3xl select-none lg:sticky lg:top-[110px]">Filters</div>
