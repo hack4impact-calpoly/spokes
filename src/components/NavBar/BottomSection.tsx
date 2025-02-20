c9736ed03434f53f74e35c0a7a29650157e48a23
Kyler <kylernord@Kylers-MacBook-Pro-2.local>
2025-02-19 16:52:31 -0800

    feat: fixed the sticky feature

--- src/components/NavBar/BottomSection.tsx
+++ src/components/NavBar/BottomSection.tsx
@@ -115,7 +115,7 @@ function useScrollDirection() {
     return () => {
       window.removeEventListener("scroll", updateScrollDirection);
     };
-  }, [scrollDirection]);
+  }, []);
 
   return scrollDirection;
 }
