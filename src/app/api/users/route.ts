28b40f9cb878f52eb3fc45b5c2ad3924f6245d9b
Ellie Pearson <ellie.jm.pearson@gmail.com>
2025-02-27 18:16:14 -0800

    refactor: post endpoint to add clerk id

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import connectDB from "@/database/db";
 import User from "@/database/userSchema"; // Import your User model
 
@@ -7,21 +8,42 @@ import User from "@/database/userSchema"; // Import your User model
 export async function POST(req: NextRequest) {
   try {
     await connectDB();
-    const { name, email, isadmin } = await req.json();
 
-    // Validate request body
-    if (!name || !email || isadmin === undefined) {
-      return NextResponse.json({ message: "Missing required fields" }, { status: 400 });
+    const { userId } = await auth();
+    if (!userId) {
+      return NextResponse.json({ message: "No user id." }, { status: 401 });
     }
 
+    console.log("user id:", userId);
+    const client = await clerkClient();
+    const clerkUser = await client.users.getUser(userId);
+    if (!clerkUser) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+
+    const { firstName, lastName } = clerkUser;
+    const email = clerkUser.primaryEmailAddress?.emailAddress || clerkUser.emailAddresses?.[0]?.emailAddress;
+    if (!email) {
+      return NextResponse.json({ message: "No email" }, { status: 404 });
+    }
+
+    const name = firstName + " " + lastName;
+
     // Check if user already exists
-    const existingUser = await User.findOne({ email });
+    const existingUser = await User.findById(userId);
     if (existingUser) {
       return NextResponse.json({ message: "User already exists" }, { status: 400 });
     }
 
     // Create new user
-    const newUser = await new User({ name, email, isadmin }).save();
+    const newUser = new User({
+      _id: userId,
+      name,
+      email: email,
+      isadmin: false,
+      postedJobs: [],
+    });
+    await newUser.save();
     return NextResponse.json(newUser, { status: 201 });
   } catch (error) {
     return NextResponse.json({ message: "Failed to create user", error }, { status: 500 });
