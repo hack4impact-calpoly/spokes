ff52b9e127e3002ce88b5361d5e0b542e00cca92
Ellie Pearson <ellie.jm.pearson@gmail.com>
2025-03-08 00:01:08 -0800

    fix: added useEffect to remove hydration error

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+import { useEffect, useState } from "react";
 import { SignIn } from "@clerk/nextjs";
 import { Center } from "@chakra-ui/react";
 
 export default function Page() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) return null; //to prevent hydration error with google auth
+
   return (
     <Center className="my-10" px={{ base: 4, md: 0 }}>
       <SignIn />
