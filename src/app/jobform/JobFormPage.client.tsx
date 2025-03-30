b6f147e81d538da1bfec377fd88094d3b0b5361d
Nolan Knievel <nolan.knievel@icloud.com>
2025-03-29 17:31:27 -0700

    fix: remove align=left from compensation type box

--- src/app/jobform/JobFormPage.client.tsx
+++ src/app/jobform/JobFormPage.client.tsx
@@ -414,7 +414,7 @@ export default function JobFormPage() {
               })}
             </Stack>
           </FormControl>
-          <Box w="full" align="left">
+          <Box w="full">
             <Collapse in={selectEmployment !== "volunteer"} animateOpacity>
               <FormControl isRequired={selectEmployment !== "volunteer"} mt={4}>
                 <FormLabel>Compensation Type</FormLabel>
