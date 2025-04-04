9026b35917bc09d6fb6ffc3bf1aeeb3928c1457b
Mark McGuire <mcguirenumber1@gmail.com>
2025-04-03 21:11:53 -0700

    refactor(simplified) - I'm proud of this

--- src/components/JobCard/JobCardInformation.tsx
+++ src/components/JobCard/JobCardInformation.tsx
@@ -1,34 +1,18 @@
-"use client";
-
 import { IJob } from "@/database/jobSchema";
-import { useEffect, useRef, useState } from "react";
 
 interface JobInformationProps {
   job: IJob;
 }
 
 export default function JobCardInformation({ job }: JobInformationProps) {
-  const industriesRef = useRef<HTMLParagraphElement>(null);
-  const [isMultiLine, setIsMultiLine] = useState(false);
-
-  useEffect(() => {
-    if (industriesRef.current) {
-      const lineHeight = parseInt(window.getComputedStyle(industriesRef.current).lineHeight);
-      const contentHeight = industriesRef.current.scrollHeight;
-      setIsMultiLine(contentHeight > lineHeight * 1.5);
-    }
-  }, [job.organizationIndustry]);
-
   return (
     <>
       <div className="mb-2">
         <h1 className="text-2xl font-bold truncate">{job.title}</h1>
         <p className="text-gray-700 font-semibold truncate">{job.organizationName}</p>
       </div>
-      <div className={`${isMultiLine ? "mb-2" : "pt-2 mb-6"}`}>
-        <p ref={industriesRef} className="text-gray-700 italic">
-          {job.organizationIndustry.join(", ")}
-        </p>
+      <div className="mb-2">
+        <p className="text-gray-700 italic">{job.organizationIndustry.join(", ")}</p>
       </div>
       <div className="mb-4">
         <p className="text-gray-700 overflow:s h-max-[150px] h-[90px] overflow-scroll no-scrollbar">
