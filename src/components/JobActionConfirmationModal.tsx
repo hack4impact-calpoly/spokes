cc54e8e7e4849ec53faa5b86d5f7c1bfd65ceaf3
Khoa <khoacuongnguyen@gmail.com>
2025-04-09 02:42:55 -0700

    feat: renamed JobDeleteConfirmationModal to JobActionCOnfirmationModal and added field to take in action. This makes this modal more reusable.

---
+++ src/components/JobActionConfirmationModal.tsx
@@ -0,0 +1,66 @@
+import { Button } from "@chakra-ui/react/button";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+} from "@chakra-ui/react";
+
+type JobActionConfirmationModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => Promise<void>;
+  action: string;
+};
+
+function JobActionConfirmationModal({ isOpen, onClose, onConfirm, action }: JobActionConfirmationModalProps) {
+  // Map action to user friendly text
+  const actionText = action;
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Confirm {actionText}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>Are you sure you would like to {actionText.toLowerCase()} this job posting?</ModalBody>
+        <ModalFooter className="flex flex-wrap gap-2 mt-4 justify-end">
+          <Button
+            px="10"
+            width="120px"
+            fontSize="small"
+            fontWeight="normal"
+            borderColor="black"
+            onClick={onConfirm}
+            sx={{
+              _hover: {
+                backgroundColor: "green.300",
+              },
+            }}
+          >
+            Confirm
+          </Button>
+          <Button
+            px="10"
+            width="120px"
+            fontSize="small"
+            fontWeight="normal"
+            borderColor="black"
+            onClick={onClose}
+            sx={{
+              _hover: {
+                backgroundColor: "red.300",
+              },
+            }}
+          >
+            Cancel
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+export default JobActionConfirmationModal;
