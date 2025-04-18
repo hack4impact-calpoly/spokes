3560a70de4463d4985cf867893c15b3477be480e
Mark McGuire <mcguirenumber1@gmail.com>
2025-04-17 18:42:24 -0700

    chore: update package-lock

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
+    "\\.(css|less|scss|sass)$": "<rootDir>/__mocks__/styleMock.js",
   },
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { configFile: "./babel.config.jest.js" }],
