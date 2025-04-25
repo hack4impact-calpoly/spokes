840b65048cf447ed87261827aa0a2cdcc145af0a
Mark McGuire <mcguirenumber1@gmail.com>
2025-04-24 17:00:02 -0700

    feat: defined routes, added checking and redirects

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,13 @@ import { NextResponse } from "next/server";
 const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 const isOnboardingRoute = createRouteMatcher(["/onboarding"]);
 const isAuthRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
-const isPublicRoute = createRouteMatcher(["/api/webhooks(.*)", "/api/users(.*)", "/jobs", "/"]);
+const isPublicRoute = createRouteMatcher([
+  "/api/webhooks(.*)",
+  "/api/users(.*)",
+  "/jobs",
+  "/api/jobs(.*)",
+  "/admin(.*)",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   // if (isAdminRoute(req)) {
@@ -15,24 +21,29 @@ export default clerkMiddleware(async (auth, req) => {
 
   const { userId } = await auth();
 
-  // If not signed in or on a public route, proceed normally
-  if (!userId || isPublicRoute(req) || isAuthRoute(req)) {
+  // if not signed in or on a public route, proceed normally
+  if (isPublicRoute(req) || isAuthRoute(req)) {
     return NextResponse.next();
+  }
+
+  if (!userId) {
+    const signInUrl = new URL("/sign-in", req.url);
+    return NextResponse.redirect(signInUrl);
   }
 
   const client = await clerkClient();
   const user = await client.users.getUser(userId);
+  console.log(userId);
 
-  // Check if user has completed onboarding
   const onboardingComplete = user.publicMetadata?.onboardingComplete === true;
 
-  // If user hasn't completed onboarding and isn't on the onboarding page, redirect them
+  // if user hasn't completed onboarding and isn't on the onboarding page, redirect them
   if (!onboardingComplete && !isOnboardingRoute(req)) {
     const onboardingUrl = new URL("/onboarding", req.url);
     return NextResponse.redirect(onboardingUrl);
   }
 
-  // If user has completed onboarding but is trying to access the onboarding page, redirect to dashboard
+  // if user has completed onboarding but is trying to access the onboarding page, redirect to dashboard
   if (onboardingComplete && isOnboardingRoute(req)) {
     const dashboardUrl = new URL("/", req.url);
     return NextResponse.redirect(dashboardUrl);
