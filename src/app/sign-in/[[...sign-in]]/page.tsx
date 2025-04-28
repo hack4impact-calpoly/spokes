3083f8334d4cb29d3d96b468b33739ba46a3326e
Khoa <khoacuongnguyen@gmail.com>
2025-04-28 08:13:02 -0700

    feat: created welcome info component and added it to sign in and sign up page

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import { SignIn } from "@clerk/nextjs";
 import { Center } from "@chakra-ui/react";
+import WelcomeInfo from "@/components/WelcomeInfo";
 
 export default function Page() {
   const [isClient, setIsClient] = useState(false);
@@ -13,7 +14,14 @@ export default function Page() {
   if (!isClient) return null; //to prevent hydration error with google auth
 
   return (
-    <Center className="my-10" px={{ base: 4, md: 0 }}>
+    <Center
+      className="my-10"
+      px={{ base: 4, md: 0 }}
+      display="flex"
+      flexDirection={{ base: "column", md: "row" }}
+      gap={10}
+    >
+      <WelcomeInfo />
       <SignIn />
     </Center>
   );
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,10 +1,18 @@
 "use client";
 import { SignUp } from "@clerk/nextjs";
 import { Center } from "@chakra-ui/react";
+import WelcomeInfo from "@/components/WelcomeInfo";
 
 export default function Page() {
   return (
-    <Center className="my-10" px={{ base: 4, md: 0 }}>
+    <Center
+      className="my-10"
+      px={{ base: 4, md: 0 }}
+      display="flex"
+      flexDirection={{ base: "column", md: "row" }}
+      gap={10}
+    >
+      <WelcomeInfo />
       <SignUp />
     </Center>
   );
