bebad1c4cc3fadaefc7b5ef14573758414cf199b
Vishnu Alachi <vishnualachi@gmail.com>
2025-05-06 10:30:45 -0700

    refactoring

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -11,21 +11,12 @@ interface AuthWithRole {
 export async function getAuthWithRole(): Promise<AuthWithRole> {
   const session = auth();
   //job-seekers will not be authenticated
-  if (!session || !(await session).userId) {
+  if (!(await session)?.userId) {
     console.log("No user found, assigning job_seeker role");
     return { userId: null, role: "job_seeker" };
   }
   const user = await session;
   const userId = user.userId;
-
-  console.log(session);
-
-  // No user logged in, default to job_seeker
-  // if (!role) {
-  //   console.log("No user found, assigning job_seeker role");
-  //   user.orgRole = "job_seeker";
-  //   return { userId: null, role: "job_seeker" };
-  // }
 
   // Check if user is part of spokes-admin organization
   const isSpokesAdmin = user.orgSlug === "spokes-admin";
@@ -33,11 +24,6 @@ export async function getAuthWithRole(): Promise<AuthWithRole> {
   if (isSpokesAdmin) {
     console.log(`User ${userId} is a spokes_admin`);
     return { userId, role: "spokes_admin" };
-  }
-
-  if (user.orgId) {
-    console.log(`User ${userId} is a nonprofit`);
-    return { userId, role: "nonprofit" };
   }
 
   // Default to nonprofit for logged-in users who aren't admins
