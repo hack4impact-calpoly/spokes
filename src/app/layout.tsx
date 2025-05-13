3527cc54c51d66f73e779c0f55f90475b82b3627
Shawn Gill <shawnsg55@gmail.com>
2025-05-13 16:03:01 -0700

    refactor: moved provids to providers.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,42 +1,26 @@
-import type { Metadata } from "next";
-import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import Providers from "./ui/providers";
 import { Inter } from "next/font/google";
 import NavBar from "@/components/NavBar/NavBar";
-import ReactQueryProvider from "@/components/ReactQueryProvider";
 import Footer from "@/components/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Spokes Job Board",
   description: "The job board is a place for nonprofit organizations to post their job openings.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider
-      appearance={{
-        variables: {
-          fontSize: "15px",
-        },
-        elements: {
-          footer: "hidden",
-        },
-      }}
-    >
-      <html lang="en">
-        <body className={inter.className}>
-          <ReactQueryProvider>
-            <Providers>
-              <NavBar />
-              {children}
-            </Providers>
-          </ReactQueryProvider>
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>
+          <NavBar />
+          {children}
           <Footer />
-        </body>
-      </html>
-    </ClerkProvider>
+        </Providers>
+      </body>
+    </html>
   );
 }
