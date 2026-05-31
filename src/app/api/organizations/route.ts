1915bf6add4ed8d6d3be9d88f782d560260ed865
Henry Orisonte <henryorisonte@MacBook-Pro-59.local>
2026-05-31 16:07:54 -0700

    feat: update organizations route to also query events and combine results

--- src/app/api/organizations/route.ts
+++ src/app/api/organizations/route.ts
@@ -1,20 +1,26 @@
 import { NextResponse } from "next/server";
 import connectDB from "@/database/db";
 import User from "@/database/userSchema";
+import Event from "@/database/eventSchema";
 import { withApiAuth } from "@/lib/auth";
+import { use } from "react";
 
 export const GET = withApiAuth(
   async () => {
     try {
       await connectDB();
 
-      const organizationNames = await User.distinct("organizationName", {
+      const userOrgs = await User.distinct("organizationName", {
+        organizationName: { $exists: true, $type: "string", $ne: "" },
+      });
+
+      const eventOrgs = await Event.distinct("organizationName", {
         organizationName: { $exists: true, $type: "string", $ne: "" },
       });
 
       const organizations = Array.from(
         new Map(
-          organizationNames
+          [...userOrgs, ...eventOrgs]
             .map((organizationName) => organizationName.trim())
             .filter(Boolean)
             .map((organizationName) => [organizationName.toLowerCase(), organizationName]),
